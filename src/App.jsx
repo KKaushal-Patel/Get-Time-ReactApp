e2191239ee607e8136ebfa9fb009ca26207cd67c
Kaushal Patel <kaushalpatel9376@gmail.com>
2021-03-22 09:40:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1 className="heading_style">
+                Welcome to <span style={{ fontWeight: "bold" }}>Slot Machine</span> game
+      </h1>
+        </>
+    );
+};
+
+export default App;
